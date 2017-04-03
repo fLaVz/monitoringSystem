e32e8cbbad13b805eeedbd7b92155977473a5cee
Delvaux Julien <juliendelvaux@neuf.fr>
2017-04-03 18:52:56 +0200

    added time loop

--- src/collecteur.py
+++ src/collecteur.py
@@ -1,5 +1,14 @@
 import psutil
 
+import time
+
+
+starttime=time.time()
+while True:
+  print "tick"
+  time.sleep(5.0 - ((time.time() - starttime) % 5.0))
+
+
 
 # USER PART
 userString = psutil.users()
