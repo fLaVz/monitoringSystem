09a18dc21cce7ce0c397d1f4e8ddee141ed9ea04
Delvaux Julien <juliendelvaux@neuf.fr>
2017-04-03 18:42:44 +0200

    Creating repository + basic collector

---
+++ src/collecteur.py
@@ -0,0 +1,52 @@
+import psutil
+
+
+# USER PART
+userString = psutil.users()
+currentUser = userString[0][0]
+
+print "\n***********************************"
+print "Current user is : " + currentUser
+print "**********************************\n"
+
+# CPU PART
+print "\n************** CPU ***************"
+
+print "Number of cores : " + str(psutil.cpu_count()) + "\n"
+
+freq = psutil.cpu_freq()
+currentFreq = freq[0]
+maxFreq = freq[1]
+minFreq = freq[2]
+
+print "Current frequecy : " + str(currentFreq) + " Mhz"
+print "Max frequency : " + str(maxFreq) + " Mhz"
+print "Min frequency : " + str(minFreq) + " Mhz"
+print "**********************************\n"
+
+
+# DISK USAGE PART
+hdd = psutil.disk_usage('/')
+
+total = str(hdd[0] / 1000000) + " Mo"
+used = str(hdd[1] / 1000000) + " Mo"
+free = str(hdd[2] / 1000000) + " Mo"
+percent = str(hdd[3]) + "%"
+
+print "\n***************** DISK USAGE ***************************"
+print "Total memory : " + total + ", Used : " + used + ", Free : " + free;
+print "Percentage of used memory : " + percent
+print "********************************************************\n"
+
+
+# MEMORY PART
+mem = psutil.swap_memory()
+
+memTot = str(mem[0] / 1000000000) + " Go"
+memUsage = str(mem[1] / 1000000) + " Mo"
+memPercent = str(mem[3]) + " %"
+
+print "\n*************** RAM **********************"
+print "Total RAM : " + memTot + ", Used : " + memUsage
+print "RAM percentage used : " + memPercent
+print "******************************************\n"
