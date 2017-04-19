8724d185fabfe2594eda0e63743600121a60763f
Delvaux Julien <juliendelvaux@neuf.fr>
2017-04-19 17:20:23 +0200

    added some architecture

--- src/collecteur/Sensor_P.py
+++ src/collecteur/Sensor_P.py
@@ -3,11 +3,12 @@ import psutil
 import time
 
 
+''' // Time function
 starttime=time.time()
 while True:
   print "tick"
   time.sleep(5.0 - ((time.time() - starttime) % 5.0))
-
+'''
 
 
 # USER PART
---
+++ src/parser.py
@@ -0,0 +1,5 @@
+from bs4 import Beautifulsoup
+
+html = urlopen("www.dominique.frin.free.fr/index.html")
+
+print html
