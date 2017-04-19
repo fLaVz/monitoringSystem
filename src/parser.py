eb4bff7f80b48df464a94809300f07ea4c6e0ea9
Delvaux Julien <juliendelvaux@neuf.fr>
2017-04-19 18:27:54 +0200

    added lib and basic parser

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
-from bs4 import Beautifulsoup
-
-html = urlopen("www.dominique.frin.free.fr/index.html")
-
-print html
+import re, urllib
+htmlSource = urllib.urlopen("http://dominique.frin.free.fr/").read(200000)
+linksList = re.findall('<a href=(.*?)>.*?</a>',htmlSource)
+for link in linksList:
+    print link
