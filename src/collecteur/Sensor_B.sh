b981a991431d9bf3b68ce0d2c4d5263f01eb855d
Benoit Dorey <benoitdorey@gmail.com>
2017-04-19 17:21:52 +0200

    Sensor Bash and Mixed

---
+++ src/collecteur/Sensor_B.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Récupérer le nombre d'utilisateurs en ligne et le nombre de processus
+
+echo " "
+echo "*************Online Users*************"
+users
+echo "**************************************"
+echo " "
+echo "***********Current Process************"
+ps -f
+echo "**************************************"
---
+++ src/collecteur/Sensor_M.py
@@ -0,0 +1,4 @@
+# Capteur mixte python qui utilise du bash
+
+import os
+os.system("free -m")
