1f94866b644d195446d7cb96dba4d5cc27725913
Delvaux Julien <juliendelvaux@neuf.fr>
2017-05-04 17:37:57 +0200

    parser and time function updated

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,9 @@
 import re, urllib
-htmlSource = urllib.urlopen("http://www.cert.ssi.gouv.fr/site/cert-fr_alerte.rss").read(20000)
-linksList = re.findall('<item><title>.*?</title>',htmlSource)
-lastAlert = linksList[0]
-lastAlert = lastAlert[13:-8]
-print lastAlert
+
+def parse():
+
+	htmlSource = urllib.urlopen("http://www.cert.ssi.gouv.fr/site/cert-fr_alerte.rss").read(20000)
+	linksList = re.findall('<item><title>.*?</title>',htmlSource)
+	lastAlert = linksList[0]
+	lastAlert = lastAlert[13:-8]
+	print lastAlert
--- src/test.py
+++ src/test.py
@@ -1,7 +1,8 @@
 import time
+import parser
 
 
 starttime=time.time()
 while True:
-  print "tick"
+  parser.parse()
   time.sleep(5.0 - ((time.time() - starttime) % 5.0))
